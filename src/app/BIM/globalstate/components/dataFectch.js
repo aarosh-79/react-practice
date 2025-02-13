9ba83445ef709f6ab03e2bf957abd21bf8de9674
aarosh-79 <aaroshmhrzn46@gmail.com>
2025-02-13 09:27:05 +0545

    third commit

--- src/app/BIM/globalstate/components/dataFectch.js
+++ src/app/BIM/globalstate/components/dataFectch.js
@@ -4,14 +4,20 @@ import axios from 'axios';
 const DataFetch = () => {
     const [product, setProduct] = useState(null);
     // const [count, setCount] = useState(0);
-    const fetchdata = () => {
-        const response = axios.get("https://fakestoreapi.com/products/1");
-        response.then((value) => {
-            setProduct(value.data)
-        })
-            .catch((error) => {
-                console.log(error, "error")
-            });
+    const fetchdata =async () => {
+        try{
+        const response = await axios.get("https://fakestoreapi.com/products/1");
+        console.log(response.data,"data")
+        }catch(error){
+            console.log(error,"error")
+        }
+        
+    //     response.then((value) => {
+    //         // setProduct(value.data)
+    //     })
+    //         .catch((error) => {addEventListener
+    //             console.log(error, "error")
+    //         });
     }
 
     useEffect(() => {
@@ -23,7 +29,7 @@ const DataFetch = () => {
             <p>
                 {product?.title}
             </p>
-            
+
         </div>
     )
 }
